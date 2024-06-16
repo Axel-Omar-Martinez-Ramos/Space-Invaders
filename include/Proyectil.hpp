66d2c3d5ca0eecea2146fb73590687725b129c2a
JPlm8 <160084632+JPlm8@users.noreply.github.com>
2024-06-16 03:20:39 -0600

    version 3

--- include/Proyectil.hpp
+++ include/Proyectil.hpp
@@ -4,34 +4,13 @@
 using namespace std;
 using namespace sf;
 
-class Proyectil : public Drawable{
-    private: 
-        Sprite sprite;
-        int vel;
-    public:
-        Proyectil(int x, int y, Texture &texture, IntRect IntRect, int velocidad_bala); /*Constaructor del proyectil*/
-        void Update();
-        Vector2f Pos();
-        virtual void draw(RenderTarget &rt, RenderStates rs) const; /*Indica como dibujar la clase Proyectil*/
+class Proyectil : public Drawable {
+	private:
+		Sprite sprite;
+		int vel;
+	public:
+		Proyectil(int x, int y, Texture &texture, IntRect intRect, int v);
+		void Update();
+		Vector2f Pos();
+		virtual void draw(RenderTarget &rt, RenderStates rs) const; 
 };
-
-Proyectil::Proyectil(int x, int y, Texture &texture, IntRect IntRect, int v){
-    sprite.setTexture(texture); /*Asignacion de la textura*/
-    sprite.setTextureRect(IntRect); /*Asignacion de textura para proyectil*/
-    sprite.setPosition(x,y); /*Escala de la textura*/
-    sprite.setScale(3,3); /*La textura es equeña, por lo tanto se expande un 300% en x,y*/
-    vel=v;
-
-}
-
-void Proyectil::Update(){
-    sprite.move(0,vel); /*Se delimita la bala a que solo se mueva en el eje y*/
-}
-
-void Proyectil::draw(RenderTarget &rt, RenderStates rs) const{
-    rt.draw(sprite,rs);
-
-}
-Vector2f Proyectil::Pos(){
-    return sprite.getPosition();
-}
